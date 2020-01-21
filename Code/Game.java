8f233effd94480829fdf6b82c40247fddef9ed48
susanshi999 <57235647+susanshi999@users.noreply.github.com>
2020-01-20 20:10:12 -0500

    Create Game.java

---
+++ Code/Game.java
@@ -0,0 +1,17 @@
+/**
+ * Starts the game of Blackjack.
+ * 
+ * @author Susan Shi
+ * @version 1.0 2018-10-29
+ */
+public class Game
+{
+    public static BlackJack game;
+    public static void main(String[] argument)
+    {
+         
+        game = new BlackJack();
+
+    } // end of method main(String[] argument)
+
+} // end of class Game
